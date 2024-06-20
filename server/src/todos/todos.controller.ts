9cc30ed560a36d82618bc6fae818e76fce4d0766
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-06-20 19:07:11 +0800

    add todos

--- server/src/todos/todos.controller.ts
+++ server/src/todos/todos.controller.ts
@@ -1,34 +1,64 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { TodosService } from './todos.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ZodValidationPipe } from 'src/pipes';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Todo, TodosService } from './todos.service';
 
 @Controller('todos')
 export class TodosController {
   constructor(private readonly todosService: TodosService) {}
 
   @Post()
-  create(@Body() createTodoDto: CreateTodoDto) {
-    return this.todosService.create(createTodoDto);
+  async create(
+    @Body(new ZodValidationPipe(CreateTodoDto)) createTodoDto: CreateTodoDto,
+  ): Promise<Todo> {
+    return await this.todosService.create(createTodoDto);
   }
 
   @Get()
-  findAll() {
-    return this.todosService.findAll();
+  async findAll(): Promise<Todo[]> {
+    return await this.todosService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.todosService.findOne(+id);
+  async findOne(@Param('id') id: string): Promise<Todo> {
+    const todo = await this.todosService.findOne(id);
+
+    if (!todo) {
+      throw new NotFoundException('Todo not found');
+    }
+
+    return todo;
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
-    return this.todosService.update(+id, updateTodoDto);
+  async update(
+    @Param('id') id: string,
+    @Body(new ZodValidationPipe(UpdateTodoDto)) updateTodoDto: UpdateTodoDto,
+  ): Promise<Todo> {
+    const todo = await this.todosService.update(id, updateTodoDto);
+
+    if (!todo) {
+      throw new NotFoundException('Todo not found');
+    }
+
+    return todo;
   }
 
+  @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.todosService.remove(+id);
+  async remove(@Param('id') id: string): Promise<void> {
+    return await this.todosService.remove(id);
   }
 }
--- server/src/todos/todos.service.ts
+++ server/src/todos/todos.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
 import { z } from 'zod';
+import { nanoid } from 'nanoid';
+import { getUnixTime } from 'date-fns';
 
 export const Todo = z.object({
   id: z.string(),
@@ -10,28 +12,65 @@ export const Todo = z.object({
   createdAt: z.number(),
   updatedAt: z.number(),
 });
-
 export type Todo = z.infer<typeof Todo>;
+
+function delay(ms = 500): Promise<void> {
+  return new Promise((res) => setTimeout(res, ms));
+}
 
 @Injectable()
 export class TodosService {
-  create(createTodoDto: CreateTodoDto) {
-    return 'This action adds a new todo';
+  private todos: Todo[] = [];
+
+  async create(createTodoDto: CreateTodoDto): Promise<Todo> {
+    await delay();
+    const now = getUnixTime(new Date());
+    const todo: Todo = {
+      ...createTodoDto,
+      id: nanoid(),
+      updatedAt: now,
+      createdAt: now,
+    };
+    this.todos.push(todo);
+
+    return todo;
   }
 
-  findAll() {
-    return `This action returns all todos`;
+  async findAll(): Promise<Todo[]> {
+    await delay();
+    return this.todos;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} todo`;
+  async findOne(id: string): Promise<Todo | undefined> {
+    await delay();
+    const todo = this.todos.find((v) => v.id === id);
+    return todo;
   }
 
-  update(id: number, updateTodoDto: UpdateTodoDto) {
-    return `This action updates a #${id} todo`;
+  async update(
+    id: string,
+    updateTodoDto: UpdateTodoDto,
+  ): Promise<Todo | undefined> {
+    await delay();
+    const index = this.todos.findIndex((v) => v.id === id);
+
+    if (index < 0) {
+      return undefined;
+    }
+
+    const todo = {
+      ...this.todos[index],
+      ...updateTodoDto,
+      updatedAt: getUnixTime(new Date()),
+    };
+    debugger;
+    this.todos[index] = todo;
+
+    return todo;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} todo`;
+  async remove(id: string): Promise<void> {
+    await delay();
+    this.todos = this.todos.filter((v) => v.id !== id);
   }
 }
