479b9687e588a293c19707900c5a8ce39ead14e2
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-07-01 22:03:17 +0800

    disable preload
    disable preload because `throw redirect()` doesn't work properly

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -12,7 +12,6 @@ const router = createRouter({
     queryClient,
     user: null,
   },
-  defaultPreload: "intent",
   // Since we're using React Query, we don't want loader calls to ever be stale
   // This will ensure that the loader is always called when the route is preloaded or visited
   defaultPreloadStaleTime: 0,
