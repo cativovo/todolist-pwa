f8d43bf22bee7e194e83c70918d4071d1721753f
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-07-02 17:48:21 +0800

    add styles to list

---
+++ client/src/components/todo-status.tsx
@@ -0,0 +1,71 @@
+import { cn } from "@/lib/utils";
+import { StatusTodo, Todo } from "@/schema/todo";
+import { ReactNode } from "@tanstack/react-router";
+import { ClassValue } from "clsx";
+import { Circle, CircleCheckBig, CircleSlash2, Timer } from "lucide-react";
+
+type TodoStatusProps = {
+  status: Todo["status"];
+  className?: ClassValue;
+};
+
+function getDisplay(status: TodoStatusProps["status"]): {
+  icon: ReactNode;
+  label: string;
+  color: string;
+} {
+  switch (status) {
+    case StatusTodo.Todo: {
+      return {
+        icon: <Timer />,
+        label: "Todo",
+        color: "#85C1E9",
+      };
+    }
+    case StatusTodo.InProgress: {
+      return {
+        icon: <Circle />,
+        label: "In Progress",
+        color: "#F8C471",
+      };
+    }
+    case StatusTodo.Done: {
+      return {
+        icon: <CircleCheckBig />,
+        label: "Done",
+        color: "#82E0AA",
+      };
+    }
+    case StatusTodo.Cancelled: {
+      return {
+        icon: <CircleSlash2 />,
+        label: "Cancelled",
+        color: "#F5B7B1",
+      };
+    }
+    default:
+      console.error(`invalid status: ${status}`);
+      return {
+        icon: null,
+        label: "",
+        color: "",
+      };
+  }
+}
+
+export default function TodoStatus(props: TodoStatusProps) {
+  const { icon, label, color } = getDisplay(props.status);
+
+  return (
+    <div
+      className={cn(
+        "flex gap-2 rounded-md px-2 py-1",
+        `bg-[${color}]`,
+        props.className,
+      )}
+    >
+      {icon}
+      {label}
+    </div>
+  );
+}
--- client/src/routes/_authenticated/index.tsx
+++ client/src/routes/_authenticated/index.tsx
@@ -1,7 +1,10 @@
 import { createTodo, findAllTodos } from "@/api/todos";
+import TodoStatus from "@/components/todo-status";
 import { findAllTodosQueryKey } from "@/query-keys";
+import { StatusTodo } from "@/schema/todo";
 import { useMutation, useSuspenseQuery } from "@tanstack/react-query";
 import { Link, createFileRoute } from "@tanstack/react-router";
+import { Loader2 } from "lucide-react";
 import { SyntheticEvent } from "react";
 
 const findAllTodosQueryOptions = {
@@ -56,14 +59,22 @@ function Index(): JSX.Element {
           <button onClick={() => query.refetch()}>retry</button>
         </>
       )}
-      <ul>
+      <ul className="mx-auto max-w-xl divide-y rounded-sm border">
         {isInProgress && (
-          <li style={{ background: "gray" }}>{mutation.variables.title}</li>
+          <li className="flex items-center gap-2 bg-gray-200 p-2">
+            <Loader2 className="h-4 w-4 animate-spin" />
+            <span className="truncate">{mutation.variables.title}</span>
+            <TodoStatus status={StatusTodo.Todo} className="ml-auto" />
+          </li>
         )}
+
         {query.data?.map((todo) => (
           <li key={todo.id}>
             <Link to="/todo/$id" params={{ id: todo.id }}>
-              {todo.title}
+              <div className="flex items-center gap-2 p-2 transition-colors hover:bg-gray-100">
+                <span className="truncate">{todo.title}</span>
+                <TodoStatus status={todo.status} className="ml-auto" />
+              </div>
             </Link>
           </li>
         ))}
