86d52a44023a5d81b8feb309abfe4d521f01af25
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-07-08 17:36:56 +0800

    update logout styles

--- client/src/routes/_authenticated.tsx
+++ client/src/routes/_authenticated.tsx
@@ -1,4 +1,11 @@
 import { logout } from "@/api/auth";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { setUser } from "@/lib/user";
 import { useMutation } from "@tanstack/react-query";
 import {
@@ -7,6 +14,7 @@ import {
   redirect,
   useRouter,
 } from "@tanstack/react-router";
+import { ChevronDown } from "lucide-react";
 
 export const Route = createFileRoute("/_authenticated")({
   component: Authenticated,
@@ -43,8 +51,24 @@ function Authenticated() {
 
   return (
     <>
-      <h1>{context.user.username}</h1>
-      <button onClick={handleLogout}>logout</button>
+      <div className="p-2 text-right">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button className="inline-flex gap-1 p-2" variant="outline">
+              <span>{context.user.username}</span>
+              <ChevronDown className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            <DropdownMenuCheckboxItem
+              className="cursor-pointer"
+              onSelect={handleLogout}
+            >
+              Logout
+            </DropdownMenuCheckboxItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
       <Outlet />
     </>
   );
