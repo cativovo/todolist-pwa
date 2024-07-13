9e8f6499a5c570ef8149f0a9edbe44bb3f1dc1c5
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-07-13 19:27:51 +0800

    make drizzle module global

--- server/src/drizzle/drizzle.module.ts
+++ server/src/drizzle/drizzle.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { drizzleProvider } from './drizzle.provider';
 import { DrizzleService } from './drizzle.service';
 
+@Global()
 @Module({
   providers: [drizzleProvider, DrizzleService],
   exports: [DrizzleService],
